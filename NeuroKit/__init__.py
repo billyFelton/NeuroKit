737560082a0d0ca59db8ba9dfc89c672e0a4cc39
Billy Felton <billy@halo.inc>
2025-11-06 13:37:58 +0000

    New atempt hoping grok keeps context and memory

--- NeuroKit/__init__.py
+++
@@ -1,9 +0,0 @@
-# NeuroKit/__init__.py
-
-__version__ = "0.1.0"
-
-from .registration import register
-from .health import start_health_server
-from .utils import get_host_ip, generate_uid, now, load_state, save_state
-from .state import get_state, set_state
-from .api import init_component
--- NeuroKit/api.py
+++
@@ -1,7 +0,0 @@
-from .registration import register
-from .health import start_health_server
-
-def init_component(service, port):
-    state = register(service, port)
-    start_health_server(port, service)
-    return state["uid"]
