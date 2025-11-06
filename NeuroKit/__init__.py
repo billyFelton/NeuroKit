ff9291fb7b1fec8c3a51bbb231b67e219583f439
Billy Felton <billy@halo.inc>
2025-11-06 12:50:22 +0000

    Fixed init

--- NeuroKit/__init__.py
+++ NeuroKit/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.1.0"
 
-from .registration import register_service
+from .registration import register
 from .health import start_health_server
 from .utils import get_host_ip, generate_uid, now, load_state, save_state
 from .state import get_state, set_state
